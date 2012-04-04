f4cae3f4d8299bd1e61bf35025276c1f27cd6ea1
Florian H <dotiweb@googlemail.com>
2012-04-04 16:16:57 +0200

    missing ',' + 3, instead of 4, spaces are now being used at indentation

--- cookie.js
+++ cookie.js
@@ -1,128 +1,131 @@
 (function (undefined) {
 
-    var isArray = Array.isArray || function (value) { // check if value is an array created with [] or new Array
-        return Object.prototype.toString.call(value) === '[object Array]';
-    },
+   var isArray = Array.isArray || function(value) { // check if value is an array created with [] or new Array
+      return Object.prototype.toString.call(value) === '[object Array]';
+   },
 
-    isPlainObj = function (value) { // check if value is an object that was created with {} or new Object
-        return Object.prototype.toString.call(value) === '[object Object]';
-    },
+   isPlainObj = function(value) { // check if value is an object that was created with {} or new Object
+      return Object.prototype.toString.call(value) === '[object Object]';
+   },
 
-    getKeys = Object.keys || function (obj) { // Object.keys polyfill
-        var keys = [], key = '';
-        for (key in obj){
-            if (obj.hasOwnProperty(key)) keys.push(key);
-        }
-        return keys;
-    },
+   getKeys = Object.keys || function(obj) { // Object.keys polyfill
+      var keys = [],
+         key = '';
+      for (key in obj) {
+         if (obj.hasOwnProperty(key)) keys.push(key);
+      }
+      return keys;
+   },
 
-    retrieve = function (value, fallback) { // return fallback if the value is undefined, otherwise return value
-        return value === undefined ? fallback : value;
-    }
-    
-     _cookie = {
-        
-        set: function (key, value, options) {
+   retrieve = function(value, fallback) { // return fallback if the value is undefined, otherwise return value
+      return value === undefined ? fallback : value;
+   },
 
-            if (isPlainObj(key)) {
-                for (var k in key) {
-                    this.set(k, key[k]);
-                }
+   _cookie = {
 
-            } else {
+      set: function(key, value, options) {
 
-                options = options || {};
-                var expires = options.expires || '',
-                    expiresType = typeof(expires),
-                    path = options.path ? ';path=' + options.path : '',
-                    domain = options.domain ? ';domain=' + options.domain : '',
-                    secure = options.secure ? ';secure' : '';
-                if (expires !== '' && expiresType == 'string') expires = ';expires=' + expires;
-                else if (expiresType == 'number') { // this is needed because IE does not support max-age
-                    var d = new Date; 
-                    d.setTime(d.getTime() + expires);
-                    expires = ';expires=' + d.toGMTString();
-                }  else if (expires.hasOwnProperty('toGMTString')) expires = ';expires=' + expires.toGMTString();
-
-                document.cookie = key + '=' + escape(value) + expires + path + domain + secure;
-
+         if (isPlainObj(key)) {
+            for (var k in key) {
+               this.set(k, key[k]);
             }
 
-            return this; // return the _cookie object to allow chaining
+         } else {
 
-        },
+            options = options || {};
+            var expires = options.expires || '',
+               expiresType = typeof(expires),
+               path = options.path ? ';path=' + options.path : '',
+               domain = options.domain ? ';domain=' + options.domain : '',
+               secure = options.secure ? ';secure' : '';
+            if (expires !== '' && expiresType == 'string') expires = ';expires=' + expires;
+            else if (expiresType == 'number') { // this is needed because IE does not support max-age
+               var d = new Date;
+               d.setTime(d.getTime() + expires);
+               expires = ';expires=' + d.toGMTString();
+            } else if (expires.hasOwnProperty('toGMTString')) expires = ';expires=' + expires.toGMTString();
 
-        remove: function (arg1) {
+            document.cookie = key + '=' + escape(value) + expires + path + domain + secure;
 
-            var keys = isArray(arg1) ? arg1 : arguments;
-            for (var i = 0, length = keys.length; i < length; i++) {
-                this.set(keys[i], '', { expires: -60 * 60 * 24 });
+         }
+
+         return this; // return the _cookie object to allow chaining
+
+      },
+
+      remove: function(arg1) {
+
+         var keys = isArray(arg1) ? arg1 : arguments;
+         for (var i = 0, length = keys.length; i < length; i++) {
+            this.set(keys[i], '', {
+               expires: -60 * 60 * 24
+            });
+         }
+
+         return this; // return the _cookie object to allow chaining
+
+      },
+
+      empty: function() {
+
+         return this.remove(getKeys(this.all())); // return the _cookie object to allow chaining
+
+      },
+
+      get: function(key, fallback) {
+
+         fallback = fallback || undefined;
+
+         if (isArray(key)) {
+
+            var result = {},
+               cookies = this.all();
+
+            for (var i = 0, l = key.length; i < l; i++) {
+               var value = key[i];
+               result[value] = retrieve(cookies[value], fallback);
             }
 
-            return this; // return the _cookie object to allow chaining
-
-        },
-
-        empty: function () {
-
-            return this.remove(getKeys(this.all())); // return the _cookie object to allow chaining
-
-        },
-
-        get: function (key, fallback) {
-
-            fallback = fallback || undefined;
-
-            if (isArray(key)) {
-
-                var result = {},
-                     cookies = this.all();
-                
-                for (var i = 0, l = key.length; i < l; i++) {
-                    var value = key[i];
-                    result[value] = retrieve(cookies[value], fallback);
-                }
-
-                return result;
+            return result;
 
 
-            } else {
+         } else {
 
-                var cookies = this.all();
-                return retrieve(cookies[key], fallback);
+            var cookies = this.all();
+            return retrieve(cookies[key], fallback);
 
-            }
+         }
 
-        },
+      },
 
-        all: function () {
+      all: function() {
 
-            if (document.cookie == '') return {};
+         if (document.cookie == '') return {};
 
-            var match = document.cookie.split('; '),
-                results = {};
-            for (var i = 0, l = match.length; i < l; i++) {
-                var tmp = match[i].split('=');
-                results[tmp[0]] = unescape(tmp[1]);
-            }
+         var match = document.cookie.split('; '),
+            results = {};
+         for (var i = 0, l = match.length; i < l; i++) {
+            var tmp = match[i].split('=');
+            results[tmp[0]] = unescape(tmp[1]);
+         }
 
-            return results;
+         return results;
 
-        }
+      }
 
-    },
+   },
 
-    cookie = function (key, fallback) { 
-        return _cookie.get(key, fallback);
-    },
+   cookie = function(key, fallback) {
+      return _cookie.get(key, fallback);
+   },
 
-    methods = ['set', 'remove', 'empty', 'get', 'all'];
+   methods = ['set', 'remove', 'empty', 'get', 'all'];
 
-    for (var i = 0, l = methods.length; i < l; i++) { // copy all _cookie methods to cookie
-        var method = methods[i];
-        cookie[method] = _cookie[method];
-    }
+   for (var i = 0, l = methods.length; i < l; i++) { // copy all _cookie methods to cookie
+      var method = methods[i];
+      cookie[method] = _cookie[method];
+   }
 
-    window.cookie = cookie;
+   window.cookie = cookie;
 
 }());
