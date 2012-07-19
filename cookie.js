22a79b487af174b8846c21db5470d8308adebb15
Florian H <dotiweb@googlemail.com>
2012-07-19 20:26:44 +0200

    Added CommonJS support. Added `cookie.defaults` and `cookie.expiryMultiplier`. Added more efficient escape method. Added strict mode

--- cookie.js
+++ cookie.js
@@ -2,138 +2,173 @@
 
 !function (document, undefined) {
 
-   var utils = {
+	'use strict';
 
-      isArray: Array.isArray || function (value) {
-         return Object.prototype.toString.call(value) === '[object Array]';
-      },
+	var utils = {
 
-      isObject: function (value) {
-         return value === Object(value);
-      },
+		// Is the given value an array? Use ES5 Array.isArray if it's available.
+		isArray: Array.isArray || function (value) {
+			return Object.prototype.toString.call(value) === '[object Array]';
+		},
 
-      toArray: function (value) { // Converts an array-like object to an array - for example `arguments`.
-         return Array.prototype.slice.call(value);
-      },
+		// Is the given value a plain object / an object whose constructor is `Object`?
+		isPlainObject: function (value) {
+			return Object.prototype.toString.call(value) === '[object Object]';
+		},
 
-      getKeys: Object.keys || function (obj) { // Get the keys of an object.
-         var keys = [],
-             key = '';
-         for (key in obj) {
-            if (obj.hasOwnProperty(key)) keys.push(key);
-         }
-         return keys;
-      },
+		// Convert an array-like object to an array – for example `arguments`.
+		toArray: function (value) {
+			return Array.prototype.slice.call(value);
+		},
 
-      retrieve: function (value, fallback) { // Returns fallback if the value is undefined, otherwise value.
-         return value === undefined ? fallback : value;
-      }
+		// Get the keys of an object. Use ES5 Object.keys if it's available.
+		getKeys: Object.keys || function (obj) {
+			var keys = [],
+				 key = '';
+			for (key in obj) {
+				if (obj.hasOwnProperty(key)) keys.push(key);
+			}
+			return keys;
+		},
 
-   };
+		// Unlike JavaScript's built-in escape functions, this method
+		// only escapes characters that are not allowed in cookies.
+		escape: function (value) {
+			return value.replace(/[,;"\\=\s%]/g, function (character) {
+				return encodeURIComponent(character);
+			});
+		},
 
-   var cookie = function () {
-      return cookie.get.apply(cookie, arguments);
-   };
+		// Return fallback if the value is undefined, otherwise return value.
+		retrieve: function (value, fallback) {
+			return value === undefined ? fallback : value;
+		}
 
-   cookie.set = function (key, value, options) {
+	};
 
-      if (utils.isObject(key)) {
+	var cookie = function () {
+		return cookie.get.apply(cookie, arguments);
+	};
 
-         for (var k in key) {
-            if (key.hasOwnProperty(k)) this.set(k, key[k]);
-         }
+	cookie.defaults = {};
 
-      } else {
+	cookie.expiresMultiplier = 60 * 60 * 24;
 
-         options = options || {};
-         var expires = options.expires || '',
-             expiresType = typeof(expires),
-             path = options.path ? ';path=' + options.path : '',
-             domain = options.domain ? ';domain=' + options.domain : '',
-             secure = options.secure ? ';secure' : '';
-         if (expiresType === 'string' && expires !== '') expires = ';expires=' + expires;
-         else if (expiresType === 'number') { // this is needed because IE does not support max-age
-            var d = new Date;
-            d.setTime(d.getTime() + 60 * 60 * 24 * expires);
-            expires = ';expires=' + d.toGMTString();
-         } else if (expires.hasOwnProperty('toGMTString')) expires = ';expires=' + expires.toGMTString();
+	cookie.set = function (key, value, options) {
 
-         document.cookie = escape(key) + '=' + escape(value) + expires + path + domain + secure;
-
-      }
-
-      return this; // to allow chaining
-
-   };
-
-   cookie.remove = function (keys) {
-
-      keys = utils.isArray(keys) ? keys : utils.toArray(arguments);
-
-      for (var i = 0, l = keys.length; i < l; i++) {
-         this.set(keys[i], '', {
-            expires: -1
-         });
-      }
+		if (utils.isPlainObject(key)) { // Then `key` contains an object with keys and values for cookies, `value` contains the options object.
 
 
-      return this; // to allow chaining
-   };
+			for (var k in key) { // TODO: `k` really sucks as a variable name, but I didn't come up with a better one yet.
+				if (key.hasOwnProperty(k)) this.set(k, key[k], value);
+			}
 
-   cookie.empty = function () {
+		} else {
 
-      return this.remove(utils.getKeys(this.all()));
+			options = utils.isPlainObject(options) ? options : { expires: options };
 
-   };
+			var expires = options.expires !== undefined ? options.expires : (this.defaults.expires || ''), // Empty string for session cookies.
+			    expiresType = typeof(expires);
 
-   cookie.get = function (keys, fallback) {
+			if (expiresType === 'string' && expires !== '') expires = new Date(expires);
+			else if (expiresType === 'number') expires = new Date(+new Date + 1000 * this.expiresMultiplier * expires); // This is needed because IE does not support the `max-age` cookie attribute.
 
-      fallback = fallback || undefined;
-      var cookies = this.all();
+			if (expires !== '' && 'toGMTString' in expires) expires = ';expires=' + expires.toGMTString();
 
-      if (utils.isArray(keys)) {
+			var path = options.path || this.defaults.path; // TODO: Too much code for a simple feature.
+			path = path ? ';path=' + path : '';
 
-         var result = {};
+			var domain = options.domain || this.defaults.domain;
+			domain = domain ? ';domain=' + domain : '';
 
-         for (var i = 0, l = keys.length; i < l; i++) {
-            var value = keys[i];
-            result[value] = utils.retrieve(cookies[value], fallback);
-         }
+			var secure = options.secure || this.defaults.secure ? ';secure' : '';
 
-         return result;
+			document.cookie = utils.escape(key) + '=' + utils.escape(value) + expires + path + domain + secure;
 
-      } else return utils.retrieve(cookies[keys], fallback);
+		}
 
-   };
+		return this; // Return the `cookie` object to make chaining possible.
 
-   cookie.all = function () {
+	};
 
-      if (document.cookie === '') return {};
+	// TODO: This is commented out, because I didn't come up with a better method name yet. Any ideas?
+	// cookie.setIfItDoesNotExist = function (key, value, options) {
+	//	if (this.get(key) === undefined) this.set.call(this, arguments);
+	// },
 
-      var cookies = document.cookie.split('; '),
-           result = {};
+	cookie.remove = function (keys) {
 
-      for (var i = 0, l = cookies.length; i < l; i++) {
-         var item = cookies[i].split('=');
-         result[item[0]] = unescape(item[1]);
-      }
+		keys = utils.isArray(keys) ? keys : utils.toArray(arguments);
 
-      return result;
+		for (var i = 0, l = keys.length; i < l; i++) {
+			this.set(keys[i], '', -1);
+		}
 
-   };
+		return this; // Return the `cookie` object to make chaining possible.
+	};
 
-   cookie.enabled = function () {
+	cookie.empty = function () {
 
-      var ret = cookie.set('a', 'b').get('a') === 'b';
-      cookie.remove('a');
-      return ret;
+		return this.remove(utils.getKeys(this.all()));
 
-   };
+	};
 
-   if (typeof define === 'function' && define.amd) {
-      define(function () {
-         return cookie;
-      });
-   } else window.cookie = cookie;
+	cookie.get = function (keys, fallback) {
+
+		fallback = fallback || undefined;
+		var cookies = this.all();
+
+		if (utils.isArray(keys)) {
+
+			var result = {};
+
+			for (var i = 0, l = keys.length; i < l; i++) {
+				var value = keys[i];
+				result[value] = utils.retrieve(cookies[value], fallback);
+			}
+
+			return result;
+
+		} else return utils.retrieve(cookies[keys], fallback);
+
+	};
+
+	cookie.all = function () {
+
+		if (document.cookie === '') return {};
+
+		var cookies = document.cookie.split('; '),
+			  result = {};
+
+		for (var i = 0, l = cookies.length; i < l; i++) {
+			var item = cookies[i].split('=');
+			result[decodeURIComponent(item[0])] = decodeURIComponent(item[1]);
+		}
+
+		return result;
+
+	};
+
+	cookie.enabled = function () {
+
+		if (navigator.cookieEnabled) return true;
+
+		var ret = cookie.set('_', '_').get('_') === '_';
+		cookie.remove('a');
+		return ret;
+
+	};
+
+	// If an AMD loader is present use AMD.
+	// If a CommonJS loader is present use CommonJS.
+	// Otherwise assign the `cookie` object to the global scope.
+
+	if (typeof define === 'function' && define.amd) {
+		define(function () {
+			return cookie;
+		});
+	} else if (typeof exports !== 'undefined') {
+		exports.cookie = cookie;
+	} else window.cookie = cookie;
 
 }(document);
