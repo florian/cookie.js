a0d9ee131e2d162baedbf37005cdf7ee96419cf6
Kohei Hasegawa <ameutau@gmail.com>
2019-07-07 22:26:21 +0900

    Use shared test for cjs and esm

--- tests/builds/cjs.js
+++ tests/builds/cjs.js
@@ -1,9 +1,12 @@
-var assert = require('assert');
-var cookie = require('../../dist/cookie.cjs.js');
+const assert = require('assert');
+const cookie = require('../../dist/cookie.cjs.js');
+const shared = require('../shared');
 
 describe('CommonJS Build', () => {
   it('imports properly', () => {
     assert(typeof cookie, 'function');
     assert(typeof cookie.get, 'function');
   })
+
+  shared.test({ cookie });
 })
